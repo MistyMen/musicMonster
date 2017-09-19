8d168eefd40fdf9ef2fc15c8df433c6873ede477
Maksym Novakh <novakhm@gmail.com>
2017-09-19 17:12:21 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+/*import all dependences*/
+const express = require('express');
+const logger = require('morgar');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+/*set port and start it listening*/
+app.listen(port, () => {
+  console.log(`listen port ${PORT}`);
+});
+
+/*set up body parser */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: false}));
+
+/*set up logger*/
+app.use(logger('dev'));
+
+/*set static file_path*/
+app.use('/static', express.static(path.json(_dirname, 'public')));
+
+
+/*set up routes*/
+app.use('/', (req, res) =>
+  res.sendFile('public/about.html');
+  )
+/*API routes*/
+const musicRoute = require('/route/tracksroutes')
+
