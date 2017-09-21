b45cef134340f1d1c655154b23b316f2b4c3673b
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-09-21 17:37:19 -0400

    Merge remote-tracking branch 'origin/dev' into dev

--- monster/db/seeds/seed.sql
+++ monster/db/seeds/seed.sql
@@ -1,7 +1,16 @@
 \c monster_dev
 
+<<<<<<< HEAD
 INSERT INTO artists (name, picture) VALUES
   ('Eminem', 'pictureUrl');
 
 INSERT INTO tracks (artist_id, song, url) VALUES
   (1, 'Lose Yourself', 'SongUrl');
+=======
+INSERT INTO artists (name, picture) VALUES 
+  ('Eminem', 'pictureUrl');
+
+INSERT INTO tracks (artist_id, song, url) VALUES 
+  (1, 'Lose Yourself', 'SongUrl');
+
+>>>>>>> 701e1810bc4dfab6425616343d3d4058d0a50ab3
