3a267b8d82cdc8fbde6b6ca6af9b9e1cd55dc15c
Jason Seminara <jseminara@gmail.com>
2017-10-02 17:35:26 -0400

    moved spacing in musicModel

--- monster/models/musicModel.js
+++ monster/models/musicModel.js
@@ -2,27 +2,26 @@ const db = require('../db/config');
 
 const Music = {};
 
-Music.findAll = () => db.many(
-  `
+Music.findAll = () => db.many(`
     SELECT id, artist, image, song, comments
     FROM records
-    `);
+`);
 
 Music.save = music => db.none(`
-    INSERT INTO records (id, artist, image, song, comments)
-    VALUES ($/id/, $/artist/, $/image/, $/song/, $/comments/)
-    ON CONFLICT (id) DO NOTHING
-    `,
-  music);
+  INSERT INTO records (id, artist, image, song, comments)
+  VALUES ($/id/, $/artist/, $/image/, $/song/, $/comments/)
+  ON CONFLICT (id) DO NOTHING
+`, music);
 
 Music.update = music => db.one(`
-    UPDATE records SET
-    comments = $/comments/
-    WHERE id = $/id/
-    RETURNING*`,
-  music);
+  UPDATE records SET
+  comments = $/comments/
+  WHERE id = $/id/
+  RETURNING *
+`, music);
 
 Music.destroy = id => db.none(`
-    DELETE FROM records WHERE id = $/id/`, id);
+  DELETE FROM records WHERE id = $/id/
+`, id);
 
 module.exports = Music;
